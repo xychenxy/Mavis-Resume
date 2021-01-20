1b979f0c3dc064ba9cef93560c18e9106cec6f50
xychenxy <mavischen916@gmail.com>
2021-01-21 09:24:19 +1100

    test

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 import UnderLine from "./Underline";
-export default function Title({title, subtitle}){
+
+export default function Title({ title, subtitle }) {
     return (
         <>
             <div className="row my-2 justify-content-center">
                 <div className="col text-center">
                     <h1 className="text-uppercase component-title">{title}</h1>
                     <h4 className="text-capitalize text-muted">{subtitle}</h4>
-                    <UnderLine/>
+                    <UnderLine />
                 </div>
             </div>
         </>
-    )
+    );
 }
